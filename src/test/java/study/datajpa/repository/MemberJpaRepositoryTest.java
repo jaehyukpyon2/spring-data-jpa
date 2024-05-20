e310bf481571031156b26a3b7ca94b388b0a4cc2
jaehyukpyon2 <ipadforjay@gmail.com>
2024-05-20 21:53:18 +0900

    basics

---
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,34 @@
+package study.datajpa.repository;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+    @Autowired
+    private MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember() throws Exception {
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        assertThat(findMember.getId()).isEqualTo(savedMember.getId());
+        assertThat(member).isEqualTo(savedMember);
+        assertThat(savedMember).isEqualTo(findMember);
+        System.out.println((member == savedMember) + ", " + (savedMember == findMember)); // true, true
+    }
+}
